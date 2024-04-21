0aeff1d4b326d752e7c47fda36ae96ce82682ef6
Awais Ali <awais.ali@uraanai.com>
2024-04-21 15:36:45 +0500

    fixes in stop vanguard file

--- stop_vanguard.sh
+++ stop_vanguard.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Stop and disable blockbook service
-sudo systemctl stop blockbook-ethereum-testnet-vanguard
-sudo systemctl disable blockbook-ethereum-testnet-vanguard
-sudo rm /etc/systemd/system/blockbook-ethereum-testnet-vanguard.service 
+sudo systemctl stop blockbook-vanar-vanguard
+sudo systemctl disable blockbook-vanar-vanguard
+sudo rm /etc/systemd/system/blockbook-vanar-vanguard.service 
 sudo systemctl daemon-reload
-sudo apt purge blockbook-ethereum-testnet-vanguard -y
+sudo apt purge blockbook-vanar-vanguard -y
 
 # Repeat for backend service
-# sudo systemctl stop backend-vanar-vanguard
-# sudo systemctl disable backend-vanar-vanguard
-# sudo rm /etc/systemd/system/backend-vanar-vanguard.service 
-# sudo systemctl daemon-reload
-# sudo apt purge backend-vanar-vanguard -y
+sudo systemctl stop backend-vanar-vanguard
+sudo systemctl disable backend-vanar-vanguard
+sudo rm /etc/systemd/system/backend-vanar-vanguard.service 
+sudo systemctl daemon-reload
+sudo apt purge backend-vanar-vanguard -y
 # 
 # # Remove installation directory
-# sudo rm -r /opt/coins/nodes/vanar_vanguard /opt/coins/data/vanar_vanguard /opt/coins/blockbook/vanar_vanguard
+sudo rm -r /opt/coins/nodes/vanar_vanguard /opt/coins/data/vanar_vanguard /opt/coins/blockbook/vanar_vanguard
